5847a3cfcce1cbc1fd0606af964f5252796b77bc
anama <BicaAnamaria>
2022-02-21 20:05:26 +0200

    Commit

---
+++ Epidem.Analysis.R
@@ -0,0 +1,38 @@
+######################
+###
+### Epidemic Simulator - Bachelor Thesis
+### Student: Anamaria Bica
+### West University, Timisoara
+### Year 3, Faculty of Computer Science
+###
+### Coordinator: Daniela Zaharie
+### Supervisor: Leonard Mada
+### Syonic SRL
+
+### based on:
+### Team Project 2021
+### Students:
+###   Dora Calea, Ioana Obreja,
+###   Liviu Sopon and Dragos Ursan
+###   West University, Timisoara
+library(deSolve)
+library(ggplot2)
+### Global options
+opt.stat.max.cutoff = 0.8; # 80% 
+
+##f1 = function(){
+#  IAll_cum = T[1] - T;
+#  Iy_cum = Sy[1] - Sy;
+#  Io_cum = So[1] - So;
+#  dIT = diff(IAll_cum);
+#  dIy = diff(Iy_cum);
+#  dIo = diff(Io_cum);
+#  maxCutoff = max(dIT) * opt.stat.max.cutoff;
+#  isHigher = (dIT >= maxCutoff);
+#  daysHigh = rle(isHigher);
+#  daysHigh = daysHigh$lengths[daysHigh$values > 0];
+#  print(daysHigh)
+##}
+#f1()
+
+
