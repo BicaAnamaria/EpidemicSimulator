9f4a9f0df9cf325d8f13c27a09823fd1da67928f
anama <BicaAnamaria>
2022-02-22 20:24:16 +0200

    Commit

--- Epidem.Analysis.R
+++ Epidem.Analysis.R
@@ -15,8 +15,7 @@
 ###   Dora Calea, Ioana Obreja,
 ###   Liviu Sopon and Dragos Ursan
 ###   West University, Timisoara
-library(deSolve)
-library(ggplot2)
+
 ### Global options
 opt.stat.max.cutoff = 0.8; # 80% 
 
@@ -26,9 +25,22 @@ summarySIR = function(x){
   IAll_cum = x$T[1] - x$T;
   Iy_cum = x$Sy[1] - x$Sy;
   Io_cum = x$So[1] - x$So;
+  Ivaccy = x$Vy[1] - x$Vy;
+  Ivacco = x$Vo[1] - x$Vo;
+  
   dIT = diff(IAll_cum);
+  strIT = paste("Infections (Total/day) =", dIT, sep=" ");
+  
   dIy = diff(Iy_cum);
+  strIy = paste("Infected Young per day =", dIy, sep=" ");
+  
   dIo = diff(Io_cum);
+  strIo = paste("Infected Old per day =", dIo, sep=" ");
+  print(strIo)
+  
+  dIvaccy = diff(Ivaccy);
+  dIvacco = diff(Ivacco);
+
   maxCutoff = max(dIT) * opt.stat.max.cutoff;
   isHigher = (dIT >= maxCutoff);
   daysHigh = rle(isHigher);
